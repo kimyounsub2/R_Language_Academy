ba63a238bbf60e9478d969aa392ac96a143fbbc5
kimyounsub <tldn0330@naver.com>
2022-05-15 09:44:52 +0900

    깃허브 테스트하기

--- test.R
+++ test.R
@@ -1,2 +1,9 @@
-tset
-a <- 1
+# tset
+# echo "# R_Language_Academy" >> README.md
+#git init
+#git add -A
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/kimyounsub2/R_Language_Academy.git
+#git push -u origin main
+
